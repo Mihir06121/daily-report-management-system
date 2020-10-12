73580d83443883f8683ca40c8f4b162722abf773
Mihir06121 <mp06121@gmail.com>
2020-10-12 19:05:35 +0530

    login and registration component created

---
+++ frontend/pages/LoginIn.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const LoginIn = () => {
+    return (
+        <Layout>
+            <h1 className="text-left pt-4 pb-4 pl-4 pr-4">Login In</h1>
+        </Layout>
+    )
+}
+
+export default LoginIn
---
+++ frontend/pages/Register.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Register = () => {
+    return (
+        <Layout>
+            <h1 className="text-left pt-4 pb-4 pl-4 pr-4">Registration</h1>
+        </Layout>
+    )
+}
+
+export default Register
